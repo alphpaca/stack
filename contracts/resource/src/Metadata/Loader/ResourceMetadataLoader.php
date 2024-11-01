ff2ecde8e65c2966a91ad2a9a26d2f9b26834930
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-11-01 12:34:57 +0100

    chore(contracts/resource): add a contract for a resource metadata loader

---
+++ contracts/resource/src/Metadata/Loader/ResourceMetadataLoader.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Eduroo (https://github.com/alphpaca/eduroo).
+ *
+ * (c) Jacob Tobiasz <jacob@alphpaca.io>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Alphpaca\Contracts\Resource\Metadata\Loader;
+
+use Alphpaca\Contracts\Resource\Metadata\ResourceMetadata;
+
+interface ResourceMetadataLoader
+{
+    public function loadFromFile(string $path): ResourceMetadata;
+
+    public function supports(string $path): bool;
+}
