9108d1786a303ffdf2f151619a894607e07d2ad5
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-11-16 09:35:27 +0100

    maintenance: add the Mockery plugin for phpstan

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -18,6 +18,7 @@ return static function (MBConfig $mbConfig): void {
             'friendsofphp/php-cs-fixer' => '^3.57',
             'mockery/mockery' => '^1.6',
             'pestphp/pest' => '^3.2',
+            'phpstan/phpstan-mockery' => '^2.0',
             'phpstan/phpstan' => '^2.0',
             'shipmonk/composer-dependency-analyser' => '^1.5',
             'symplify/monorepo-builder' => '^11.2',
