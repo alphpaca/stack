2fb9946a8d7ac12b0bb52b45d5588250998adeda
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2025-01-05 14:47:43 +0100

    feat(resource): define a basic contract for a resource action

---
+++ contracts/resource/src/Action/Result.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alphpaca\Contracts\Resource\Action;
+
+/**
+ * A marker interface for discriminating action results.
+ *
+ * @since 0.1
+ */
+interface Result
+{
+}
